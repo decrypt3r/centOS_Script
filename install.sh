d86408e6c32360188528e462bff8682e64dd5443
Shubham Kotwal <shubhamkotwal1225@gmail.com>
2019-08-24 20:27:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ echo "shubh3 > /tmp/logs.log" >> /etc/bashrc
 echo "shubh3 >> /tmp/logs1.log" >> /etc/bashrc
 echo "sendmail shubhamk.esds@gmail.com < /tmp/logs.log" >> /etc/bashrc
 echo "shubh" >> /etc/bashrc
+yum install ntp
 systemctl enable ntpd
 systemctl restart ntpd
 ntpdate -q 0.centos.pool.ntp.org
